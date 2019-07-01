b2fb79dd69686eca94d5cbed1f2856c501949baf
bufferbox <38717946+bufferbox@users.noreply.github.com>
2019-07-01 13:00:36 +0300

    page for code documentation

--- exampl1.py
+++ exampl1.py
@@ -1,9 +1,11 @@
 def func1(arg1, arg2):
     """
     This is a simple function
+
     :param arg1: value
     :param arg2: another value
-    :return:
+
+    :return: arg1
     """
     arg1 = arg2
     return arg1
--- sphinx/index.rst
+++ sphinx/index.rst
@@ -10,6 +10,7 @@ Welcome to LSTM documentation website's documentation!
    :maxdepth: 2
    :caption: Contents:
 
+   code
 
 
 Indices and tables
